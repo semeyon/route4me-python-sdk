ced2a24662af29f22626948702d62a3f5158e8a5
Semeyon <semeyon@svetliy.ru>
2018-08-27 19:45:34 +0600

    Update api_endpoints.py

--- route4me/api_endpoints.py
+++ route4me/api_endpoints.py
@@ -1,5 +1,5 @@
 DRIVER_VERSION = 'route4me-python-driver-0.0.1'
-MAIN_HOST = 'https://broono'
+MAIN_HOST = 'https://api.broono'
 
 ACTIVITY_FEED = '{0}/api.v4/activity_feed.php'.format(MAIN_HOST)
 ADDRESSBOOK = '{0}/api.v4/address_book.php'.format(MAIN_HOST)
